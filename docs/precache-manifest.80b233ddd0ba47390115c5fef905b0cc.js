74d1b294b1013f511006919655f81a052761796d
TheSniker007 <joaojl2099@gmail.com>
2020-06-18 21:28:01 -0300

    build to github pages

---
+++ docs/precache-manifest.80b233ddd0ba47390115c5fef905b0cc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2281168c2e736fbba641049f1b1a285d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acab925e05659d1bb901",
+    "url": "/static/css/main.6374703d.chunk.css"
+  },
+  {
+    "revision": "887718a0a0156febc194",
+    "url": "/static/js/2.46bb34a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46bb34a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acab925e05659d1bb901",
+    "url": "/static/js/main.72c3c0cf.chunk.js"
+  },
+  {
+    "revision": "3097492c5ae1c532e21d",
+    "url": "/static/js/runtime-main.0a502514.js"
+  },
+  {
+    "revision": "18fd3f6f1a6879d60b471eb4828bd890",
+    "url": "/static/media/040.18fd3f6f.wav"
+  },
+  {
+    "revision": "84925ab144bdb2a4d7a2a3669643f830",
+    "url": "/static/media/041.84925ab1.wav"
+  },
+  {
+    "revision": "217b943a565a4c60e452aac2852fd0e9",
+    "url": "/static/media/042.217b943a.wav"
+  },
+  {
+    "revision": "0bca30d9849c36cbd3d59f40f0ce233c",
+    "url": "/static/media/043.0bca30d9.wav"
+  },
+  {
+    "revision": "1f26c778a049229d1c7a2edfb789b5fd",
+    "url": "/static/media/044.1f26c778.wav"
+  },
+  {
+    "revision": "a787534ad7a68707ce5c7657c0a26985",
+    "url": "/static/media/045.a787534a.wav"
+  },
+  {
+    "revision": "deb0f09aeb56dfe2f7fd7a705509a295",
+    "url": "/static/media/046.deb0f09a.wav"
+  },
+  {
+    "revision": "785a33d2df8ac21f71be64df8f8f579a",
+    "url": "/static/media/047.785a33d2.wav"
+  },
+  {
+    "revision": "6ff99ed4f2e58de57da7430e54a31dc8",
+    "url": "/static/media/048.6ff99ed4.wav"
+  },
+  {
+    "revision": "10af03f3aa8a23a3e29cab5bb9f7d48f",
+    "url": "/static/media/049.10af03f3.wav"
+  },
+  {
+    "revision": "840651acbc526fe58fffbfd6429f2aad",
+    "url": "/static/media/050.840651ac.wav"
+  },
+  {
+    "revision": "70924435d531fe3a2c59d6596733c5cc",
+    "url": "/static/media/051.70924435.wav"
+  },
+  {
+    "revision": "25454bf446227aa40165fb1a99a062c2",
+    "url": "/static/media/052.25454bf4.wav"
+  },
+  {
+    "revision": "dd6d8182b57f130ca005307db4573008",
+    "url": "/static/media/053.dd6d8182.wav"
+  },
+  {
+    "revision": "ecc42d0a36052cbb619cccb1dc3871e4",
+    "url": "/static/media/054.ecc42d0a.wav"
+  },
+  {
+    "revision": "d84c47da4c6a48f67c2a6d2792851d3d",
+    "url": "/static/media/055.d84c47da.wav"
+  },
+  {
+    "revision": "dcfa00d82aa25eb54383f8b4f3ec749f",
+    "url": "/static/media/056.dcfa00d8.wav"
+  }
+]);
